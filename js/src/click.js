a6b4440c26caf6df798f4643f9df0bcc13ccd2f0
Macintosh HD <macintoshhd@MacintoshdeMacBook-Pro.local>
2019-08-08 23:09:39 +0800

    Site updated: 2019-08-08 23:09:39

---
+++ js/src/click.js
@@ -0,0 +1,28 @@
+/* 鼠标特效 */
+var a_idx = 0;
+jQuery(document).ready(function($) {
+  $("body").click(function(e) {
+    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "奶子😂");
+    var $i = $("<span />").text(a[a_idx]);
+    a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
+      y = e.pageY;
+    $i.css({
+      "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+      "top": y - 20,
+      "left": x,
+      "position": "absolute",
+      "font-weight": "bold",
+      "color": "#f7794d"
+    });
+    $("body").append($i);
+    $i.animate({
+        "top": y - 180,
+        "opacity": 0
+      },
+      1500,
+      function() {
+        $i.remove();
+      });
+  });
+});
